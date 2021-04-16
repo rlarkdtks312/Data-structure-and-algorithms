2faf2ebe4de2b3b8ed0ece22b5258f9dd9fcc83b
rlarkdtks312 <rkdtks3783@naver.com>
2021-04-16 20:27:32 +0900

    연결 리스트로 구현한 스택 upload

---
+++ 연결 리스트로 구현한 스택.c
@@ -0,0 +1,76 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int element;
+typedef struct {
+	element data;
+	struct StackNode* link;
+} StackNode;
+
+//스택의 top을 표시해주기 위한 구조체
+typedef struct { //처음에 보고 당황했지만 이름이 없어도 별명으로 부를수 있는것처럼 이름을 생략해도 됩니다. 
+	StackNode* top;
+}LinkedStackType;
+
+// 초기화 함수
+void init(LinkedStackType* s)
+{
+	s->top = NULL;
+}
+
+// 공백 상태 검출 함수
+int is_empty(LinkedStackType* s)
+{
+	return (s->top == NULL);
+}
+// 포화 상태 검출 함수
+int is_full(LinkedStackType* s)
+{
+	return 0;
+}
+// 삽입 함수
+void push(LinkedStackType* s, element item)
+{
+	StackNode* temp = (StackNode*)malloc(sizeof(StackNode));
+	temp->data = item;
+	temp->link = s->top;
+	s->top = temp;
+}
+void print_stack(LinkedStackType* s)
+{
+	for (StackNode* p = s->top; p != NULL; p = p->link)
+		printf("%d->", p->data);
+	printf("NULL \n");
+}
+
+// 삭제 함수
+element pop(LinkedStackType* s)
+{
+	if (is_empty(s)) {
+		fprintf(stderr, "스택이 비어있음\n");
+		exit(1);
+	}
+	else {
+		StackNode* temp = s->top;
+		int data = temp->data;
+		s->top = s->top->link;
+		free(temp);
+		return data;
+	}
+}
+
+// 주 함수
+int main(void)
+{
+	LinkedStackType s;
+	init(&s);
+	push(&s, 1); print_stack(&s);
+	push(&s, 2); print_stack(&s);
+	push(&s, 3); print_stack(&s);
+	pop(&s); print_stack(&s);
+	pop(&s); print_stack(&s);
+	pop(&s); print_stack(&s);
+	return 0;
+}
