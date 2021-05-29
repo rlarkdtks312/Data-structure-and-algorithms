61b01227031f08e96ea34775c5802bde41e1e02e
rokboo <rokboo@naver.com>
2021-05-29 18:22:37 +0900

    PBL 13주차 과제 소스코드입니다.

---
+++ 알고리즘 문제/PBL 가운데 말하기.cpp
@@ -0,0 +1,87 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 100000
+
+typedef struct Heap {//힙 구조체 선언
+    int heap_size;
+    int heap[MAX_SIZE];
+}Heap;
+
+Heap* create()//힙 생성
+{
+    return (Heap*)malloc(sizeof(Heap));
+}
+
+void init(Heap* h)//힙 초기화 함수
+{
+    h->heap_size = 0;
+}
+
+void insert_heap(Heap* h, int data)//힙에 노드를 추가하는 함수
+{
+    int i;
+    i = ++(h->heap_size);
+    while ((i != 1) && (data > h->heap[i / 2])) {// 트리를 거슬러 올라가면서 부모 노드와 비교
+        h->heap[i] = h->heap[i / 2];//부모 노드의 값보다 클 경우 부모 노드와 위치를 바꿈
+        i /= 2;
+    }
+    h->heap[i] = data; // 새로운 노드를 추가
+}
+
+int delete_heap(Heap* h)//힙의 루트를 삭제하는 함수
+{
+    int parent, child;
+    int data, temp;
+    data = h->heap[1];//돌려줄 힙의 루트 값을 저장
+    temp = h->heap[(h->heap_size)--];//힙의 크기를 줄이고 마지막으로 추가된 노드를 저장
+    parent = 1;
+    child = 2;
+    while (child <= h->heap_size) {
+        if ((child < h->heap_size) && (h->heap[child]) < h->heap[child + 1])// 현재 노드의 자식노드 중 더 큰 자식노드를 찾음
+            child++;
+        if (temp >= h->heap[child]) break;// temp가 자식노드들보다 클 경우 반복문을 빠져나감
+        h->heap[parent] = h->heap[child];//부모와 자식 노드의 위치를 바꿈
+        parent = child;//트리의 다음 높이로 내려감
+        child *= 2;
+    }
+    h->heap[parent] = temp;//temp값을 parent 위치에 삽입
+    return data;//힙의 루트값을 돌려줌
+}
+
+int middle(Heap* h) {//힙의 중간값을 찾는 함수
+    int s = h->heap_size;//힙의 크기를 저장
+    int* sort = (int*)malloc(sizeof(int) * s);//힙을 정렬할 sort를 동적할당
+    Heap* heap1;//새로운 힙을 생성
+    heap1 = create();
+    init(heap1);
+    for (int i = 0; i < s; i++) {//입력받은 힙을 그대로 새로운 힙에 복사
+        insert_heap(heap1, h->heap[i+1]);
+    }
+    for (int i = s-1; i >= 0; i--) {//힙의 루트들을 제거하면서 작은수가 처음에 오도록 sort에 값을 저장
+        sort[i] = delete_heap(heap1);
+    }
+    int mid = sort[(s-1) / 2];//저장된 값의 중간값을 mid에 저장
+    free(sort);//동적할당 해제
+    free(heap1);
+    return mid;//mid값을 돌려줌
+}
+
+int main(void){
+    int data;
+    int num = 0;
+    Heap* heap;//힙 생성
+    heap = create();
+    init(heap);
+    scanf_s("%d", &num);//입력받을 수의 개수를 입력받음
+    int* ans = (int*)malloc(sizeof(int) * num);//가운데 수를 저장할 ans를 동적할당
+    for (int i = 0 ; i < num; i++) {//num만큼 수를 입력받고 입력받을 때마다 가운데 값을 ans에 저장
+        scanf_s("%d", &data);
+        insert_heap(heap, data);
+        ans[i] = middle(heap);
+    }
+    printf("가운데 값\n");
+    for (int i = 0; i < heap->heap_size; i++) {//저장된 가운데 값들을 출력
+        printf("%d\n", ans[i]);
+    }
+    free(ans);//동적할당 해제
+}
---
+++ 알고리즘 문제/PBL 이진 검색 트리.cpp
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {//노드 구조체 선언
+	int data;
+	struct node* left, * right;
+}node;
+
+node* new_node(int data)//노드를 생성하는 함수
+{
+	node* temp = (node*)malloc(sizeof(node));//노드를 동적할당
+	temp->data = data;//주어진 데이터값을 삽입
+	temp->left = temp->right = NULL;//왼쪽,오른쪽 트리 초기화
+	return temp;//생성한 노드를 돌려줌
+}
+
+node* insert_node(node* node, int key)//이진 탐색 트리에 값을 추가하는 함수
+{
+	if (node == NULL) return new_node(key);//주소 값이 NULL일 경우 새 노드를 생성함
+	if (key < node->data)//입력받은 값이 부모 노드의 데이터보다 작을 경우 왼쪽트리에 값을 추가
+		node->left = insert_node(node->left, key);
+	else if (key > node->data)//입력받은 값이 부모 노드의 데이터보다 클 경우 오른쪽트리에 값을 추가
+		node->right = insert_node(node->right, key);
+	return node;//값을 삽입한 후 부모 노드를 돌려줌
+}
+
+void postorder(node* root){//트리를 후위순회 하는 함수
+	if (root) {
+		postorder(root->left);//왼쪽부터 후위순회
+		postorder(root->right);//오른쪽을 후위순회
+		printf("%d\n", root->data);//순회가 종료된 후 데이터를 출력
+	}
+}
+
+int main(void)
+{
+	node* root = NULL;//트리의 루트를 NULL로 선언
+	int num = 0;//트리의 노드 개수 선언
+	int data = 0;//노드에 삽입할 데이터값 선언
+	scanf_s("%d", &num);//노드 개수를 입력받음
+	for (int i = 0; i < num; i++) {//노드 개수만큼 이진 탐색트리에 값을 추가
+		scanf_s("%d", &data);
+		root = insert_node(root, data);
+	}
+	printf("후위순회 결과\n");
+	postorder(root);//이진탐색트리 후위순회
+}
